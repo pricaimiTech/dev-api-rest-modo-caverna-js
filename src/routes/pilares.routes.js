42a0dd38df86db99a29c4649c1396089bdd11d38
Priscila Caimi <priscila.caimi@hotmail.com>
2025-04-09 21:55:55 -0300

    criando controller para pilar

--- src/routes/pilares.routes.js
+++ src/routes/pilares.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Pilar from "../../models/Pilar.js";
+import PilarController from "../../controllers/pilarController.js";
 const router = express.Router();
 
 
@@ -13,15 +13,7 @@ const router = express.Router();
  *       200:
  *         description: Lista de pilares
  */
-router.get("/", async(req, res) => {
-    try {
-        const pilares = await Pilar.find();
-        res.status(200).json(pilares);
-    } catch (error) {
-        res.status(500).json({ message: "Erro ao buscar pilares", error });
-        
-    }
-});
+router.get("/", PilarController.getAll);
 
 
 /**
@@ -43,16 +35,7 @@ router.get("/", async(req, res) => {
  *       404:
  *         description: Pilar não encontrado
  */
-router.get("/:id", async(req, res) => {
-    try {
-        const pilar = await Pilar.findById(req.params.id);
-        if (!pilar) return res.status(404).json({ message: "Pilar não encontrado" });
-        res.status(200).json(pilar);
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao buscar pilar", error });
-      }
-    
-});
+router.get("/:id", PilarController.getById);
 
 /**
  * @swagger
@@ -73,59 +56,6 @@ router.get("/:id", async(req, res) => {
  *       404:
  *         description: Nenhum pilar encontrado
  */
-router.get("/objetivo/:nome", async(req, res) => {
-      try {
-        const nome = req.params.nome.toLowerCase();
-        const pilares = await Pilar.find({ 'objetivo.title': nome }); // busca todos os objetivos com o status informado
-    
-        if (!pilares || pilares.length === 0) {
-          return res.status(404).json({ message: "Nenhum Pilar encontrado com esse status" });
-        }
-    
-        res.status(200).json(pilares);
-      } catch (error) {
-        res.status(500).json({
-          message: "Erro ao buscar pilar pelo nome",
-          error: error.message,
-        });
-      }
-});
-
-/**
- * @swagger
- * /pilares/objetivo/id/{id}:
- *   get:
- *     summary: Lista pilares por ID do objetivo
- *     tags: [Pilares]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID do objetivo
- *     responses:
- *       200:
- *         description: Lista de pilares encontrados
- *       404:
- *         description: Nenhum pilar encontrado
- */
-router.get("/objetivo/id/:id", async(req, res) => {
-    try {
-        const objetivo = req.params.id;
-        const pilares = await Pilar.find({ "objetivo.id": objetivo }); // busca pilares que tenha o id do objetivo vinculado
-    
-        if (!pilares || pilares.length === 0) {
-          return res.status(404).json({ message: "Nenhum pilar encontrado com esse status" });
-        }
-    
-        res.status(200).json(pilares);
-      } catch (error) {
-        res.status(500).json({
-          message: "Erro ao buscar pilar pelo status",
-          error: error.message,
-        });
-      }
-});
+router.get("/objetivo/:nome", PilarController.getByObjetivo);
 
 export default router;
